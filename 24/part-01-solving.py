a9fddbf2a0e04995179d98826bdfa34f12e29241
Onslauth <onslauth@gmail.com>
2021-12-26 22:07:59 +0200

    master: adding day 24
    Adding solutions for day 24.

---
+++ 24/part-01-solving.py
@@ -0,0 +1,82 @@
+
+X = [ 13, 15, 15, 11, -7, 10, 10, -5, 15, -3, 0,  -5, -9, 0  ]
+Y = [ 6,  7,  10, 2,  15, 8,  1,  10, 5,  3,  5,  11, 12, 10 ]
+Z = [ 1,  1,  1,  1,  26, 1,  1,  26, 1,  26, 26, 26, 26, 26 ]
+
+z.push( A + 6 )
+z.push( B + 7 )
+z.push( C + 10 )
+z.push( D + 2 )
+
+E != z.pop( ) - 7
+  z.push( E + 15 )
+
+z.push( F + 8 )
+z.push( G + 1 )
+
+H != z.pop( ) - 5
+  z.push( H + 10 )
+
+z.push( I + 5 )
+
+J != z.pop( ) - 3
+  z.push( J + 3 )
+
+K != z.pop( ) - 0
+  z.push( K + 5 )
+
+L != z.pop( ) - 5
+  z.push( L + 11 )
+
+M = z.pop( ) - 9
+  z.push( M + 12 )
+
+N = z.pop( ) - 0
+  z.push( N + 10 )
+
+
+
+z.push( A + 6 )
+z.push( B + 7 )
+
+M = z.pop( ) - 9
+  z.push( M + 12 )
+
+N = z.pop( ) - 0
+  z.push( N + 10 )
+
+
+E = D + 2 - 7  = D - 5
+H = G + 1 - 5  = G - 4
+J = I + 5 - 3  = I + 2
+K = F + 8 - 0  = F + 8
+L = C + 10 - 5 = C + 5
+M = B + 7 - 9  = B - 2
+N = A + 6 - 0  = A + 6
+
+E = D - 5
+H = G - 4
+J = I + 2
+K = F + 8
+L = C + 5
+M = B - 2
+N = A + 6
+
+A = 3
+B = 9
+C = 4
+D = 9
+E = 4
+F = 1
+G = 9
+H = 5
+I = 7
+J = 9
+K = 9
+L = 9
+M = 7
+N = 9
+
+
+
+
